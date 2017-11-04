db99c83a14c085a260b4118f1e50b528ff73e3e5
ivane gomarteli <ivanejg@yahoo.com>
2017-11-04 18:55:54 +0200

    fix broken feature specs

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.feature "listing exercises" do 
 	before do
-		@john = User.create(email: "john@example.com", password: "password")
+		@john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
 		login_as(@john)
 
 		@ex1 = @john.exercises.create(duration_in_min: 20, workout: "My body building activity", workout_date: Date.today)
--- spec/features/hiding_signing_signup_links_spec.rb
+++ spec/features/hiding_signing_signup_links_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Hidding sign in  sign up links' do
 	before do
-   @john = User.create!(email: "john@example.com", password: "password")
+   @john = User.create!(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
  	end
 
  	scenario "upon successful signin" do 
