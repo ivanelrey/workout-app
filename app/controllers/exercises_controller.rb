450f0e43321ca8aae7ae1d68d557a58c032710cd
ivane gomarteli <ivanejg@yahoo.com>
2017-12-24 23:57:59 +0000

    implement messages controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,8 +1,13 @@
 class ExercisesController < ApplicationController
 	before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@exercises = current_user.exercises
 		@friends = current_user.friends
+		set_current_room
+		@message = Message.new
+		@messages = current_room.messages if current_room
+		@followers = Friendship.where(friend_id: current_user.id)
 	end
 
 	def show
@@ -51,5 +56,14 @@ class ExercisesController < ApplicationController
 
 	def exercise_params
 		params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+	end
+
+	def set_current_room
+		if params[:room_id]
+			@room = Room.find_by(id: params[:room_id])
+		else
+			@room = current_user.room
+		end
+		session[:current_room] = @room.id if @room
 	end
 end
